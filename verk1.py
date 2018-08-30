38456dba3b28819334bae9c421019f221d3d27e2
ivarthor <33898539+ivarthor@users.noreply.github.com>
2018-08-30 14:34:53 +0000

    Add files via upload
    heroku

---
+++ verk1.py
@@ -0,0 +1,27 @@
+from sys import argv
+from bottle import route, run, template
+
+@route('/')
+def index():
+    return """
+    <h2> Verkefni 1 </h2>
+    <p><a href=" /about"> um okkur </a></p>
+    <p><a href=" /bio"> ferilskra</a></p>
+    <p><a href=" /pics"> Myndir</a></p>
+    """
+
+@route('/about')
+def jobbi():
+    return "hér eru upplýsingar um Jonna vinnumann"
+
+@route('/bio')
+def jobbi():
+    return "hér er ferilskrá Jonna vinnumanns"
+
+@route('/pics')
+def jobbi():
+    return "hér eru myndir af Jonna vinnumann"
+
+
+
+run(host='0.0.0.0', port=agrv[1])
